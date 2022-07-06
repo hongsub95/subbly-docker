508a720105b5254c03a9a624f8615d1af9717f57
hongsub <hong9506@gmail.com>
2022-07-06 23:04:32 +0900

    first commit

---
+++ clothes/forms.py
@@ -0,0 +1,19 @@
+from django.forms import ModelForm
+from . import models as clothes_models
+from options import models as options_models
+from markets import models as markgets_models
+
+
+class SearchForm(ModelForm):
+    class Meta:
+        model = clothes_models.Clothes
+        fields = ("name",)
+
+
+
+class ContactForm(ModelForm):
+
+    class Meta:
+        model = clothes_models.Clothes
+        fields = '__all__'
+    
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode:"jit",
+  prefix:"t-",
+  content: ["./**/templates/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
