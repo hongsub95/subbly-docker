62589941939811c2c8c46844063a0c3ad5e58abc
hongsub <hong9506@gmail.com>
2022-07-27 00:39:13 +0900

    AWS RDS mariadb

--- config/settings/common.py
+++ config/settings/common.py
@@ -63,10 +63,13 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME":os.environ.get("DB_NAME"),
-        "USER": "root",
+        "USER": "admin",
         "PASSWORD":os.environ.get("DB_PWD"),
-        "HOST": "localhost",
+        "HOST": "subbly-db.cq026ec0kbij.ap-northeast-2.rds.amazonaws.com",
         "PORT": "3306",
+        'OPTIONS':{
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     }
 }
 
