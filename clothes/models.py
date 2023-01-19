bb929c3df3f12eb889d3801cc67eec8b2ee4de7f
hongsub <hong9506@gmail.com>
2023-01-20 03:53:51 +0900

    ELK 설정2

--- clothes/models.py
+++ clothes/models.py
@@ -65,7 +65,7 @@ class Clothes(core_models.TimeStampedModel):
         return reverse("clothes:clothes_detail", kwargs={"pk": self.pk})
 
 #내부용
-class Product(models.Model):
+class Product(core_models.TimeStampedModel):
     clothes = models.ForeignKey("clothes",on_delete=models.DO_NOTHING)
     name = models.CharField(max_length=100, verbose_name="상품명")
     
